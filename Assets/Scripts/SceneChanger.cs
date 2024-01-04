0cffb8298ba44b024d95222044abf2b0467a18c8
yong-gyun <kihara05110605@gmail.com>
2024-01-04 19:43:25 +0900

    시작 컷씬 연결

---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+
+    public string sceneName;
+
+    void Start()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
