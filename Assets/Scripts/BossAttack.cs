a7ac840fe38098a8227f8e69006da97ec3c68c66
pji2918 <son77391@gmail.com>
2024-01-04 14:38:32 +0900

    병합 오류 수정

--- Assets/Scripts/BossAttack.cs
+++ Assets/Scripts/BossAttack.cs
@@ -1,7 +1,8 @@
 using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.XR.Interaction.Toolkit;
+using TMPro;
+using TextTyping;
 
 public class BossAttack : MonoBehaviour
 {
@@ -14,14 +15,15 @@ public class BossAttack : MonoBehaviour
     public GameObject skill3;
     public GameObject skill4;
 
-    public Text Bosstext;
-    public Text PlayerText;
+    public TextMeshProUGUI Bosstext;
+    public TextMeshProUGUI PlayerText;
 
     public GameObject[] Reporter;
 
     public Transform zapu;
 
     public GameObject Portal;
+    Typing typing = new Typing();
 
     bool action = false;
     bool rull2Running = false; // 추가된 변수
@@ -60,11 +62,12 @@ public class BossAttack : MonoBehaviour
 
     IEnumerator Rull2()
     {
+        AudioSource playerAudioSource = PlayerText.transform.parent.GetComponent<AudioSource>();
         rull2Running = true; // 코루틴이 시작되었음을 표시
         yield return new WaitForSeconds(3f);
-        PlayerText.text = "보스를 처치하는데 성공했어!";
+        StartCoroutine(typing.TypeText("보스를 처치하는데 성공했어!", PlayerText, playerAudioSource));
         yield return new WaitForSeconds(3f);
-        PlayerText.text = "빠르게 포탈로 이동해보자";
+        StartCoroutine(typing.TypeText("빠르게 포탈로 이동해보자", PlayerText, playerAudioSource));
         yield return new WaitForSeconds(3f);
         PlayerText.text = "";
         action = false;
@@ -73,19 +76,21 @@ public class BossAttack : MonoBehaviour
 
     IEnumerator Rull()
     {
+        AudioSource playerAudioSource = PlayerText.transform.parent.GetComponent<AudioSource>();
+        AudioSource bossAudioSource = Bosstext.transform.parent.GetComponent<AudioSource>();
         yield return new WaitForSeconds(1f);
         player = GameObject.FindGameObjectWithTag("Player").transform;
         yield return new WaitForSeconds(3f);
         PortalController portalController = FindObjectOfType<PortalController>();
-        Bosstext.text = "감히 우리본진을 터트려??";
+        StartCoroutine(typing.TypeText("감히 우리본진을 터트려??", Bosstext, bossAudioSource));
         yield return new WaitForSeconds(3f);
-        Bosstext.text = "죽여버리겠다!!";
+        StartCoroutine(typing.TypeText("죽여버리겠다!!", Bosstext, bossAudioSource));
         yield return new WaitForSeconds(1f);
         Bosstext.text = "";
         yield return new WaitForSeconds(5f);
-        PlayerText.text = "적의 보스는 보호막을 뚫는스킬들이야";
+        StartCoroutine(typing.TypeText("적의 보스는 보호막을 뚫는스킬들이야", PlayerText, playerAudioSource));
         yield return new WaitForSeconds(3f);
-        PlayerText.text = "최대한 스킬을 피한후 보스가 지키고있는 라이트닝을 부셔야해!";
+        StartCoroutine(typing.TypeText("최대한 스킬을 피한후 보스가 지키고있는 라이트닝을 부셔야해!", PlayerText, playerAudioSource));
 
         if (portalController != null)
         {
