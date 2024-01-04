a902770fb8d94deb1c165d9bff52b1d94e200454
yong-gyun <kihara05110605@gmail.com>
2024-01-05 04:06:00 +0900

    오탈자 및 맞춤법 수정, 약간의 대사 변경

--- Assets/Scripts/LobbyText1.cs
+++ Assets/Scripts/LobbyText1.cs
@@ -20,13 +20,13 @@ public class LobbyText1 : MonoBehaviour
     {
         AudioSource audioSource = Gamerule.transform.parent.GetComponent<AudioSource>();
         yield return new WaitForSeconds(3);
-        StartCoroutine(typing.TypeText("지금으로부터 300년전 가재왕이 우리 기지인 코스모넛 행성을 정복하였다", Gamerule, audioSource));
+        StartCoroutine(typing.TypeText("지금으로부터 300년전 가재왕에게 우리 기지인 코스모넛 행성을 정복당했다.", Gamerule, audioSource));
         yield return new WaitForSeconds(6);
         StartCoroutine(typing.TypeText("정복을 당하면서 많은 군함을 뺏기고 나에겐 단 하나의 군함과 우주선만이 남았다..", Gamerule, audioSource));
         yield return new WaitForSeconds(6);
         StartCoroutine(typing.TypeText("나는 복수를 위해 우주선을 개발해왔고 드디어 오늘 적군의 기지를 공격하려한다.", Gamerule, audioSource));
         yield return new WaitForSeconds(6);
-        StartCoroutine(typing.TypeText("이제 적진으로 출발하겠다 오바", Gamerule, audioSource));
+        StartCoroutine(typing.TypeText("이제 적진으로 출발할것이다.", Gamerule, audioSource));
         yield return new WaitForSeconds(3);
         Gamerule.text = "";
         if (fadeImage == null)
